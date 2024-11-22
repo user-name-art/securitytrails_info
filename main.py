2a3dae49c070b25878fccf33c47ce02aa702e50a
Art Gilyazov <art.gilyazov@mail.ru>
2024-11-22 12:19:08 +0300

    add comments.

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import argparse
 from dotenv import load_dotenv
 
 
-VERSION = 'v1.00'
+VERSION = 'v1.01'
 
+# кастомизация подсветки
 red = '\x1b[1;31m'
 green = '\x1b[1;32m'
 yellow = '\x1b[1;33m'
 blue = '\x1b[1;34m'
 white = '\x1b[1;37m'
 
+# доступные записи и их соответсвтие ключам в ответе securitytrails.
 avaliable_record_types = {
     'a': 'ip',
     'ns': 'nameserver',
@@ -19,12 +21,14 @@ avaliable_record_types = {
 }
 
 
+# Функция делает запрос к securitytrails и получает ответ.
 def request_info(url, headers):
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
 
 
+# Функция выводит ответ в консоль.
 def print_domain_history(domain_history, field, record_type):
     if domain_history['records']:
         for records in domain_history['records']:
@@ -38,7 +42,7 @@ def print_domain_history(domain_history, field, record_type):
 
 def main():
     load_dotenv()
-    api_key = os.environ.get('API_KEY')
+    api_key = os.environ.get('API_KEY')  # получаем ключ из .env файла.
 
     headers = {
         "accept": "application/json",
@@ -47,13 +51,13 @@ def main():
 
     parser = argparse.ArgumentParser(prog='alias_name', description=f'Script for securitytrails {VERSION}')
     parser.add_argument('domain', type=str, help='Домен (без слэшей и протоколов)')
-    parser.add_argument('record_type', type=str, nargs='?', default='a', help='Тип записи A, NS или MX')
+    parser.add_argument('record_type', type=str, nargs='?', default='a', help='Тип записи (A, NS или MX)')
 
     domain = parser.parse_args().domain
     record_type = parser.parse_args().record_type
-    url = f'https://api.securitytrails.com/v1/history/{domain}/dns/{record_type}'
+    url = f'https://api.securitytrails.com/v1/history/{domain}/dns/{record_type}'  # Формируем ссылку для запроса.
 
-    if record_type in avaliable_record_types.keys():
+    if record_type in avaliable_record_types.keys():  # Прежде чем сделать запрос, проверяем, что тип записи поддерживается.
         try:
             domain_history = request_info(url=url, headers=headers)
             field = avaliable_record_types[record_type]
